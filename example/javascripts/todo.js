69e628d8719608714301e6cbf98a359953485c95
Felix Flores <felix@edgecase.com>
2012-01-27 00:41:18 -0500

    Brainstorming glui api.

--- example/javascripts/todo.js
+++ example/javascripts/todo.js
@@ -4,20 +4,29 @@ var Task = function(desc) {
   this.editMode = false;
 };
 
-var TodoList = function() {
-  this.tasks = [];
-};
+Task.render = _.template("<b><%- value %></b>");
 
-// $(function() {
-//   var mList = new Glue(new TodoList)
-//     , vList = new Glui(mList);
-// 
-//   vList.$('#new-task').submit(function(event, glue, data) {
-//     glue.push('tasks', new Task(data.description));
-//   });
-// 
-//   vlist.addListener('tasks[]', 'push,remove', function(msg) {
-//     this.render({ description: msg.newValue });
-//   });
-// });
+$(function() {
+  var g = new Glui(new Glue([]));
+
+  g.$('#new-task').enter(function(e, el, data) {
+    this.push(new Task(data.description));
+  });
+
+  g.$('#task .task').dblclick(function(e, el, data, index) {
+    this.set('[' + index + '].editMode', !this.target[index].editMode);
+  });
+
+  g.addListener('[]:push', function(msg) {
+    $('#tasks').insert(Task.render({ task: msg.newValue }));
+  });
+
+  g.addListener('[]:remove', function(msg) {
+    $('#task .task').eq(index).remove();
+  });
+
+  g.addListener('[]:set', function(msg) {
+    $('#task .task').eq(msg.index).html(Task.render({ task: msg.newValue }));
+  });
+});
 
